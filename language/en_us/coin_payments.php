7d0d7b13606badde1cc31968833de3c9364b7682
Temporary <Temporary@Paul-XPS>
2018-01-05 14:31:34 -0800

    Initial commit

---
+++ language/en_us/coin_payments.php
@@ -0,0 +1,7 @@
+<?php
+$lang['CoinPayments.name'] = "CoinPayments.net";
+
+$lang['CoinPayments.merchant_id'] = "CoinPayments Merchant ID";
+$lang['CoinPayments.ipn_secret'] = "IPN Secret";
+$lang['CoinPayments.buildprocess.submit'] = "Pay with CoinPayments";
+?>
